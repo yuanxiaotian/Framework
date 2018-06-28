ee24ff5b46babd502534d533bcf9aa78b76ab17e
potato <864738352@qq.com>
2018-06-28 09:27:45 +0800

    修改baseFragment,添加管理fragment方法

--- Library/src/main/java/com/cangmaomao/lib/base/BaseFragment.kt
+++ Library/src/main/java/com/cangmaomao/lib/base/BaseFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cangmaomao.lib.utils.SystemUtils
 import com.tbruyelle.rxpermissions2.RxPermissions
+import me.yokeyword.fragmentation.ISupportFragment
 
 
 /**
@@ -14,12 +14,11 @@ import com.tbruyelle.rxpermissions2.RxPermissions
 
 abstract class BaseFragment<T : BasePresenter> : BaseAppCompatFragment(), BaseView<T> {
 
-    var t: T? = null
+    lateinit var t: T
     var mRxPermissions: RxPermissions? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        t = SystemUtils.getGenericInstance(this, 0)
         mRxPermissions = RxPermissions(_mActivity)
     }
 
@@ -39,5 +38,48 @@ abstract class BaseFragment<T : BasePresenter> : BaseAppCompatFragment(), BaseVi
 
 
     abstract fun initView(savedInstanceState: Bundle?)
+
+
+    /****
+     * 从栈中移除当前fragment
+     */
+    override fun pop() {
+        super.pop()
+    }
+
+    /****
+     * 跳转至目标fragment
+     */
+    override fun start(toFragment: ISupportFragment?) {
+        super.start(toFragment)
+    }
+
+    /***
+     * toFragment：目标fragment
+     * targetFragmentClass:当前fragment
+     * includeTargetFragment:是否在栈中保留当前fragment
+     */
+    override fun startWithPopTo(toFragment: ISupportFragment?, targetFragmentClass: Class<*>?, includeTargetFragment: Boolean) {
+        super.startWithPopTo(toFragment, targetFragmentClass, includeTargetFragment)
+    }
+
+
+    /****
+     * 加载fragment
+     * containerId：加载的id
+     * toFragment:被加载的fargment对象
+     */
+    override fun loadRootFragment(containerId: Int, toFragment: ISupportFragment?) {
+        super.loadRootFragment(containerId, toFragment)
+    }
+
+
+    /****
+     * 替换fragment
+     */
+    override fun replaceFragment(toFragment: ISupportFragment?, addToBackStack: Boolean) {
+        super.replaceFragment(toFragment, addToBackStack)
+    }
+
 
 }
